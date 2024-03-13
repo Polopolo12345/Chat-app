fed5d91ee0cde0afb1f53d98404461e8f0729f6f
John <carltran2005@gmail.com>
2024-03-12 23:30:46 -0400

    2nd part completed - UI desgin

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,35 @@
+import Conversation from "./Conversation";
+
+const Conversations = () => {
+    return (
+    <>
+        <div className="flex flex-col py-2 overflow-auto">
+            <Conversation/>
+            <Conversation/>
+            <Conversation/>
+            <Conversation/>
+            <Conversation/>
+        </div>
+    </>
+    );
+};
+
+export default Conversations;
+
+// import Conversation from "./Conversation";
+
+// const Conversations = () => {
+//     return (
+//     <>
+//         <div className="flex flex-col py-2 overflow-auto">
+//             <Conversation/>
+//             <Conversation/>
+//             <Conversation/>
+//             <Conversation/>
+//             <Conversation/>
+//         </div>
+//     </>
+//     );
+// };
+
+// export default Conversations;
